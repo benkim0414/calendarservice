de6c6bb9420b28232c1f6f77e125692dca25f694
Gunwoo Kim <gunwoo@gunwoo.org>
2020-03-24 15:36:08 +1100

    Configure zap logger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,26 +8,46 @@ import (
 
 	calendarpb "github.com/benkim0414/calendarservice/pb"
 	"github.com/benkim0414/calendarservice/pkg/calendarservice"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("failed to build zap logger: %v", err)
+	}
+	defer logger.Sync()
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":50051"))
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		logger.Fatal("failed to listen: %v", zap.Error(err))
 	}
 
 	ctx := context.Background()
 	svc, err := calendarservice.New(ctx)
 	if err != nil {
-		log.Fatalf("failed to initialize the service: %v", err)
+		logger.Fatal("failed to initialize the service: %v", zap.Error(err))
 	}
 
-	s := grpc.NewServer()
+	grpc_zap.ReplaceGrpcLoggerV2(logger)
+	s := grpc.NewServer(
+		grpc_middleware.WithUnaryServerChain(
+			grpc_ctxtags.UnaryServerInterceptor(),
+			grpc_zap.UnaryServerInterceptor(logger),
+		),
+		grpc_middleware.WithStreamServerChain(
+			grpc_ctxtags.StreamServerInterceptor(),
+			grpc_zap.StreamServerInterceptor(logger),
+		),
+	)
 	calendarpb.RegisterCalendarServer(s, svc)
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+		logger.Fatal("failed to serve: %v", zap.Error(err))
 	}
 }
